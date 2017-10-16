00c557f3b341aa3981fe6ebc9ca8bb162d0a7600
Claire <clmaffei@gmail.com>
2017-10-16 10:21:17 +0200

    level 1

--- level1/main.rb
+++ level1/main.rb
@@ -1,3 +1,36 @@
 require "json"
 
-# your code
+
+class PlanningDeGarde
+  def initialize(dataFile, outputFile)
+    @data = JSON.parse(File.read(dataFile))
+    @output = outputFile
+  end
+
+  def perform
+    schedule = {}
+    schedule["workers"] = get_schedule
+    File.open(@output,"w") do |f|
+      f.write(schedule.to_json)
+    end
+  end
+
+  private
+    def get_schedule
+      @data["shifts"].
+      group_by {|shift| shift["user_id"]}.
+      map do |user_id, shifts|
+        {
+          "id" => user_id,
+          "price" => select_user(user_id)["price_per_shift"]*shifts.count
+        }
+      end
+    end
+
+    def select_user(user_id)
+      @data["workers"].select{|worker| worker["id"] == user_id}[0]
+    end
+
+end
+
+PlanningDeGarde.new('data.json', 'output.json').perform
